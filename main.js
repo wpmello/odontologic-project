27d7ae09bb1575cc5e9812a00d1da01976986e93
wpmello <wagnermello903@gmail.com>
2022-05-03 19:52:04 -0300

    UPDATE CSS and add some functions with javascript

---
+++ main.js
@@ -0,0 +1,15 @@
+function onScroll() {
+  if (scrollY === 0) {
+    navigation.classList.remove('scroll')
+  } else {
+    navigation.classList.add('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
